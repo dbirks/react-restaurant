ef5ced94468856b5f32d5fefee5dde7f89ed760e
David Birks <david@birks.dev>
2022-09-30 10:51:23 -0400

    Switch to react-query

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,40 +1,35 @@
-import { useEffect, useState } from "react";
-import { Food } from "./food";
+import { useQuery } from "@tanstack/react-query";
 import { getFoods } from "./services/foodsApi";
 import Heading from "./shared/Heading";
 
 export default function Menu() {
-  const [foods, setFoods] = useState<Food[]>([]);
+    const foodQuery = useQuery(["foods"], getFoods);
 
-  useEffect(() => {
-    async function fetchFoods() {
-      setFoods(await getFoods());
-    }
-    fetchFoods();
-  }, []);
+    if (foodQuery.isLoading) return <p>Loading...</p>;
+    if (foodQuery.isError) throw foodQuery.error;
 
-  return (
-    <>
-      <Heading level={2}>Menu</Heading>
-      <div className="flex flex-wrap">
-        {foods.map((food) => {
-          return (
-            <div
-              key={food.name}
-              className="hover:bg-indigo-600 hover:text-white p-2 shadow-lg border max-w-sm m-2 rounded"
-            >
-              <Heading level={3}>{food.name}</Heading>
-              <img
-                className="h-52"
-                src={"/images/" + food.image}
-                alt={food.name}
-              />
-              <p>{food.description}</p>
-              <p>${food.price}</p>
+    return (
+        <>
+            <Heading level={2}>Menu</Heading>
+            <div className="flex flex-wrap">
+                {foodQuery.data.map((food) => {
+                    return (
+                        <div
+                            key={food.name}
+                            className="hover:bg-indigo-600 hover:text-white p-2 shadow-lg border max-w-sm m-2 rounded"
+                        >
+                            <Heading level={3}>{food.name}</Heading>
+                            <img
+                                className="h-52"
+                                src={"/images/" + food.image}
+                                alt={food.name}
+                            />
+                            <p>{food.description}</p>
+                            <p>${food.price}</p>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </>
-  );
+        </>
+    );
 }
